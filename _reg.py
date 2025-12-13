520085756b0283147d4d8f0358e64b7bd37fd8e5
Elian <elianmedina2005@gmail.com>
2025-12-12 20:59:06 -0400

    Ultimo commit hasta horita

--- _reg.py
+++ _reg.py
@@ -61,14 +61,9 @@ text = "Todo el mundo dice que la IA nos va a quitar el trabajo. Pero la ia no e
 pattern = "IA"
 found = re.findall(pattern, text, re.IGNORECASE)
 
-<<<<<<< HEAD
 
 if found:
     print(found)
 
 else:
     print("Not found")
-=======
-if found:
-    print(found)
->>>>>>> f89975e (Rama nueva)
