277110ea209e6ec645c1c3385de314a75af7ffaf
Elian <elianmedina2005@gmail.com>
2025-12-12 22:34:00 -0400

    02 - Meta caracteres

--- _reg.py
+++ _reg.py
@@ -1,7 +1,7 @@
 # # Expresiones regulares
 # 1-Import re
 from ast import pattern
-from operator import le
+
 import re
 
 # # 2- Crear un patron, que es una cadena dque describe lo que queremos encontrar
@@ -57,6 +57,8 @@ for i in result:
     print(i.group(), i.start(), i.end())
 
 
+# Buscar con distincion de mayusculas y minusculas
+
 text = "Todo el mundo dice que la IA nos va a quitar el trabajo. Pero la ia no es tan mala. ¡Viva la Ia!"
 pattern = "IA"
 found = re.findall(pattern, text, re.IGNORECASE)
@@ -67,3 +69,81 @@ if found:
 
 else:
     print("Not found")
+
+
+### Reemplazar el texto
+
+# .sub() reemplaza todas las coincidencias de un patrón en un texto
+
+
+# text = 'Cherry-pick es como "copiar y pegar" un commit específico de una rama a otra o como duplicar.'
+
+# pattern = "como"
+
+# replacement = "like"
+
+# new = re.sub(pattern, replacement, text, flags=re.IGNORECASE, count=3)
+
+# print(new)
+
+###
+# 02 - Meta caracteres
+# Los metacaracteres son simbolos especiales con significados especificos en las expresiones regulares
+###
+
+# 1- punto
+
+# Coincidir con cualquier caracter excepto una linea (r)
+
+text = """El niño dijo "hola", luego gritó "holass", después escribió "holla" y finalmente suspiró "hona" cuando quería decir "haya"."""
+
+pattern = r"ho.a"  # prefijo (r)
+
+found = re.findall(pattern, text)
+
+if found:
+    print(found)
+else:
+    print("Palabra no encontrada.")
+
+# Barra invertida para buscar el punto(.)
+
+text = "Mi vida es buena. La tuya tambien..."
+
+pattern = r"\."
+
+found = re.findall(pattern, text)
+
+if found:
+    print(found)
+
+else:
+    print("Lo sentimos...")
+##############################################################
+
+# \d: coincide con cualquier dígito (0-9)
+
+text = "El número de teléfono es 123456789"
+found = re.findall(r"\d{9}", text)
+
+print(found)
+
+# Ejercicio: Detectar si hay un número de España en el texto gracias al prefijo +34
+
+text = "Mi número de teléfono es +1 688999999 apúntalo vale?"
+pattern = r"\+1 \d{9}"
+
+found = re.search(pattern, text)
+
+if found:
+    print(f"El numero: {found.group()}, es un numero de RD")
+else:
+    print("El numero no es de RD")
+
+
+# \w: Coincide con cualquier caracter alfanumerico (a-z, A-Z, 0-9, _)
+
+text = "el_rubius_69"
+pattern = r"\w"
+found = re.findall(pattern, text)
+print(found)
